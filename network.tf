a346b12211a581e2b2889365c44edf156b0efbb8
Maria <maria.martinezbarrio@gmail.com>
2022-01-26 00:11:59 +0000

    Added network module

---
+++ network.tf
@@ -0,0 +1,12 @@
+//--------------------------------------------------------------------
+// Variables
+variable "network_resource_group_name" {}
+
+//--------------------------------------------------------------------
+// Modules
+module "network" {
+  source  = "app.terraform.io/MARIA/network/azurerm"
+  version = "3.5.0"
+
+  resource_group_name = "${var.network_resource_group_name}"
+}
