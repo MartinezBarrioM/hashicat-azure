72e7198e42cb49aed9b4aefbf10b368a83ffec92
Maria <maria.martinezbarrio@gmail.com>
2022-01-26 00:27:07 +0000

    Added network module

--- main.tf
+++ main.tf
@@ -19,18 +19,6 @@ resource "azurerm_resource_group" "myresourcegroup" {
     environment = "Production"
   }
 }
-//--------------------------------------------------------------------
-// Variables
-variable "network_resource_group_name" {}
-
-//--------------------------------------------------------------------
-// Modules
-module "network" {
-  source  = "app.terraform.io/MARIA/network/azurerm"
-  version = "3.5.0"
-
-  resource_group_name = "${var.network_resource_group_name}"
-}
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.prefix}-vnet"
